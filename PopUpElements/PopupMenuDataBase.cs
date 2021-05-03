3e2b0b6092b0aa8653a4967bb1c763a930a50b30
Henrik Langer Hallenberg Schou <henrik-langer@live.dk>
2021-05-03 14:53:42 +0200

    Addet PupUp Elements

---
+++ PopUpElements/PopupMenuDataBase.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EksternealElements.PopUpElements
+{
+    public class PopupMenuDataBase
+    {
+        public virtual bool DisableMainClass { get; set; } = false;
+        public virtual string Class { get; set; }
+        public virtual Action<RenderTreeBuilder> RenderFreagment { get; set; }
+        public virtual Action RefreshSender { get; set; }
+        public Action Refres { get; internal set; }
+    }
+}
---
+++ PopUpElements/PupupMenu.razor.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace EksternealElements.PopUpElements
+{
+    public partial class PupupMenu
+    {
+        [Parameter]
+        public string Class { get; set; } 
+        [Inject]
+        PopupMenuDataBase Data { get; set; }
+        RenderFragment Fragment { get; set; }
+
+        protected override void OnInitialized()
+        {
+            Fragment = CreatCompunent();
+            Data.Refres = StateHasChanged;
+        }
+        private RenderFragment CreatCompunent() => builder => Data.RenderFreagment(builder);
+    }
+}
