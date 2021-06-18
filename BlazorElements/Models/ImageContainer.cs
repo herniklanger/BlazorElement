978ab3ec575f3c4e779c4b6252a60c1f79eead7b
Henrik Langer Hallenberg Schou <henrik-langer@live.dk>
2021-06-19 01:49:45 +0200

    Start Roling Image

---
+++ BlazorElements/Models/ImageContainer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EksternealElements.Models
+{
+    public class ImageContainer
+    {
+        public string ImgaeLocation { get; set; }
+        public string RoutingUrl { get; set; }
+    }
+}
