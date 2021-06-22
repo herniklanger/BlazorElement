6566dbf99cca61d07c378290ba3b41be636cc462
Henrik Langer Hallenberg Schou <henrik-langer@live.dk>
2021-06-22 11:01:52 +0200

    Add Extension for tree builder

---
+++ BlazorElements/RenderTreeBuilderExtensions/TreeBuilderEkstensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.CompilerServices;
+using Microsoft.AspNetCore.Components.Rendering;
+
+namespace BlazorEkstensions.RenderTreeBuilderExtensions
+{
+    public static class TreeBuilderEkstensions
+    {
+        public static void BindValue<TValue>(this RenderTreeBuilder builder, object resiver, ref int sequence, TValue value,EventCallback<TValue> eventCallback, string valueName = "Value")
+        {
+            builder.AddAttribute(sequence++, valueName, RuntimeHelpers.TypeCheck<TValue>(value));
+            builder.AddAttribute(sequence++, valueName+"Changed", RuntimeHelpers.TypeCheck<EventCallback<TValue>>(
+    EventCallback.Factory.Create<TValue>(resiver, eventCallback)));
+
+        }
+    }
+}
