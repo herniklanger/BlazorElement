8cfb405dfacd867e9dbc73952e13595d33fd1def
Henrik Langer <42315828+herniklanger@users.noreply.github.com>
2021-07-03 23:40:52 +0200

    Add Tabs

---
+++ BlazorElements/Nave/Tab.razor.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EksterneElements.Nave
+{
+    public partial class Tab
+    {
+        [Parameter]
+        public string Name { get; set; }
+        [Parameter]
+        public TabControler perrent { get; set; }
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+        [Parameter]
+        public bool Default { get; set; } = false;
+        private bool aktive
+        {
+            get
+            {
+                if (perrent?.TabVisulize == null)
+                {
+                    return false;
+                }
+                return perrent.TabVisulize == ChildContent;
+            }
+        }
+        protected override void OnInitialized()
+        {
+            if(perrent.TabVisulize == null && Default)
+            {
+                perrent.TabVisulize = ChildContent;
+            }
+        }
+    }
+}
